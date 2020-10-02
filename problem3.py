5e3f22d5b0da69c44eb1d49b122961bee7081650
Peter <PeterC499@deltalearns.ca>
2020-10-02 12:43:48 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -3,4 +3,4 @@ n =int(input("Gimme a numnber "))
 a=0
 for x in range (1,n+1):
         a = a + int("1" * x)
-print("The sum of the series is " + str(a))
+print("the sum of the series is " + str(a))
