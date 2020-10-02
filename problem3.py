3ea689779d46f08a0f87c7d10ef9cdc24e1cf45a
Peter <PeterC499@deltalearns.ca>
2020-10-02 12:42:28 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -1,8 +1,6 @@
 #! python3
-num=[50]
 n =int(input("Gimme a numnber "))
-for x in range (1,n):
-    num[x+1] = num[x]+10**x
-    
-    
-print(y)
+a=0
+for x in range (1,n+1):
+        a = a + int("1" * x)
+print("The sum of the series is " + str(a))
