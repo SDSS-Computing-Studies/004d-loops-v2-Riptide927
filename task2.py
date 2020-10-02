7fcaa2ad1e094e8730eb1ed5ef0411e3917df290
Peter <PeterC499@deltalearns.ca>
2020-10-02 12:03:41 -0700

    ggg

--- task2.py
+++ task2.py
@@ -3,7 +3,7 @@ name = input("What is your name ").strip()
 coolnames= ("Lebron","Kobe","Michale","Shaq","Dennis")
 for x in coolnames:
     if name == x:
-        print("That name is in the list")
+        print("That name is on the list")
         break
 if name not in coolnames:
-    print("That name is not in the list")
+    print("That name is not on the list")
--- task3.py
+++ task3.py
@@ -2,4 +2,4 @@
 num= (25, 8, 10, 11, 33, 30, 51, 75, 63, 14, 20, 99)
 for y in num:
     if y%5==0:
-        print(y, end=" ")
+        print(y)
